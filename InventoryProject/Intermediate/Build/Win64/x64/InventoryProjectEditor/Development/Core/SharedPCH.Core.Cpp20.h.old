7840a9da40d72ff72373e039f90051a832036cba
Shane Hall <149245031+Shane1585@users.noreply.github.com>
2024-10-25 13:03:08 +0100

    new inventory ui

---
+++ InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Core/SharedPCH.Core.Cpp20.h.old
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "F:/Unreal/InventoryPlugin/InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
--- InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
+++ InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "F:/Unreal/InventoryPlugin/InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
+#include "C:/Users/D3524091/Documents/GitHub/InventoryPlugin/InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
 #include "Runtime/Engine/Public/EngineSharedPCH.h"
