fc0c260c9743911e2fbe77487cef119c2284d1ee
shane hall <D3524091@live.tees.ac.uk>
2024-10-29 22:05:19 +0000

    Fixed up some UI issues

--- InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++ InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/D3524091/Documents/GitHub/InventoryPlugin/InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
+#include "F:/Unreal/InventoryPlugin/InventoryProject/Intermediate/Build/Win64/x64/InventoryProjectEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
 #include "Runtime/Core/Public/CoreSharedPCH.h"
