3d85af56245c9604172ffc0abecbdaa34662641b
shane hall <D3524091@live.tees.ac.uk>
2024-11-27 21:00:25 +0000

    Picking up items now works

--- Plugins/InventorySystem/Source/InventorySystem/Private/GroundItem.cpp
+++ Plugins/InventorySystem/Source/InventorySystem/Private/GroundItem.cpp
@@ -4,6 +4,7 @@
 #include "GroundItem.h"
 
 #include "GroundItemDetail.h"
+#include "InventoryComponent.h"
 
 #include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
 
@@ -13,13 +14,21 @@ AGroundItem::AGroundItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Set up the initial data for the overlap box, to make it attached to the actor, put it in the right place,
+	// make it collide with only pawns and to generate overlap events.
+	this->InteractTriggerBox->SetupAttachment(this->RootComponent);
+	this->InteractTriggerBox->SetRelativeTransform(FTransform());
+	this->InteractTriggerBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	this->InteractTriggerBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	this->InteractTriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	this->InteractTriggerBox->SetGenerateOverlapEvents(true);
 }
 
 // Called when the game starts or when spawned
 void AGroundItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -31,17 +40,30 @@ void AGroundItem::Tick(float DeltaTime)
 
 void AGroundItem::InitialiseItem(FTransform Transform, FGroundItemDetail NewActorDetails, FInventorySlot NewInventoryDetails)
 {
-
+	// sets up mobility, transform and scale of the actor
 	this->SetMobility(EComponentMobility::Movable);
 	this->SetActorTransform(Transform);
 	this->GetStaticMeshComponent()->SetRelativeScale3D(NewActorDetails.ItemScale);
-	
-	UStaticMesh* Mesh = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), this, *NewActorDetails.StaticMeshPath));
 
+	// adds the mesh to the actor to make it look like the item
+	UStaticMesh* Mesh = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), this, *NewActorDetails.StaticMeshPath));
 	this->GetStaticMeshComponent()->SetStaticMesh(Mesh);
-	
-	this->InteractTriggerBox->SetBoxExtent(FVector(NewActorDetails.InteractAreaScale));
-	
+
+	// sets up the details if picked up into inventory
 	this->InventoryDetails = NewInventoryDetails;
+
+	// sets the trigger box size and adds an overlap event to this actor
+	this->InteractTriggerBox->SetBoxExtent(FVector(NewActorDetails.InteractAreaScale));
+	this->OnActorBeginOverlap.AddDynamic(this, &AGroundItem::PickupItem);
+
 }
+
+void AGroundItem::PickupItem(AActor* OverlappedActor, AActor* OtherActor)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Overlapping"));
+	UInventoryComponent* Inventory = Cast<UInventoryComponent>(OtherActor->GetComponentByClass(UInventoryComponent::StaticClass()));
+	Inventory->AddItem(InventoryDetails.Item, InventoryDetails.Amount);
+	this->Destroy();
+}
+
 
--- Plugins/InventorySystem/Source/InventorySystem/Public/GroundItem.h
+++ Plugins/InventorySystem/Source/InventorySystem/Public/GroundItem.h
@@ -27,7 +27,10 @@ public:
 	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item details")
 	FInventorySlot InventoryDetails;
-	
+
+	/**
+	 * The trigger box for interaction
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item appearence")
 	UBoxComponent* InteractTriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractTriggerBox"));
 	
@@ -39,9 +42,18 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	/**
+	 * Sets up the actor to be in the world and interacted with.
+	 * This is not in the constructor due to UE5 limits. Construct the actor, then call this.
+	 * 
+	 * @param Transform Where to place the item
+	 * @param NewActorDetails What item has been dropped (the look and size)
+	 * @param NewInventoryDetails Item details when picked up (number, weight, name, etc.)
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Drop and pickup")
 	void InitialiseItem(FTransform Transform, FGroundItemDetail NewActorDetails, FInventorySlot NewInventoryDetails);
-	
-	
+
+	UFUNCTION(BlueprintCallable, Category = "Drop and pickup")
+	void PickupItem(AActor* OverlappedActor, AActor* OtherActor);
 
 };
