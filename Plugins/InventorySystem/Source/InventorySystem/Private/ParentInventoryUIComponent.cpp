1add24c7eef959d4ae2a3ea4eafd08768596de4b
shane hall <D3524091@live.tees.ac.uk>
2024-12-05 17:21:59 +0000

    test

---
+++ Plugins/InventorySystem/Source/InventorySystem/Private/ParentInventoryUIComponent.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ParentInventoryUIComponent.h"
+
+// Sets default values for this component's properties
+UParentInventoryUIComponent::UParentInventoryUIComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UParentInventoryUIComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UParentInventoryUIComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UParentInventoryUIComponent::EnableInventoryUI(UInventoryComponent* InventoryRepresented)
+{
+}
+
+void UParentInventoryUIComponent::DisableInventoryUI()
+{
+}
+
---
+++ Plugins/InventorySystem/Source/InventorySystem/Public/ParentInventoryUIComponent.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryComponent.h"
+#include "Components/ActorComponent.h"
+#include "ParentInventoryUIComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
+class INVENTORYSYSTEM_API UParentInventoryUIComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UParentInventoryUIComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(Blueprintable, BlueprintCallable, Category = "Item interaction")
+	void EnableInventoryUI(UInventoryComponent* InventoryRepresented);
+	
+	UFUNCTION(Blueprintable, BlueprintCallable, Category = "Item interaction")
+	void DisableInventoryUI();
+};
